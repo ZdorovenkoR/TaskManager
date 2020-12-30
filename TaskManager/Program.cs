8751b185292b19a124a0291fddb7414620f6a0f6
Roman Zdorovenko <75687097+ZdorovenkoR@users.noreply.github.com>
2020-12-30 22:31:06 +0600

    Update Program.cs
    //Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и
    //позволяет завершить указанный процесс. Предусмотреть возможность завершения процессов с
    //помощью указания его ID или имени процесса.

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+
+//Написать консольное приложение Task Manager, которое выводит на экран запущенные процессы и
+//позволяет завершить указанный процесс. Предусмотреть возможность завершения процессов с
+//помощью указания его ID или имени процесса. 
 
 namespace TaskManager
 {
@@ -10,6 +15,20 @@ namespace TaskManager
     {
         static void Main(string[] args)
         {
+            foreach (Process process in Process.GetProcesses())
+            {
+                Console.WriteLine($"ID: {process.Id} Name: {process.ProcessName}");
+            }
+            Console.WriteLine("Введите ID процесса который необходимо завершить");
+            var idProces = Convert.ToInt32(Console.ReadLine());
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (idProces == process.Id)
+                {
+                    process.Kill();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
